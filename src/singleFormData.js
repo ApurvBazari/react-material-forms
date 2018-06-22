9b192bbeee8c46f1fefcb88ef395b1d12671091f
Apurv Bazari <apurv.bazari@bookmyshow.com>
2018-06-22 12:57:26 +0530

    Starting modularizing components

--- src/singleFormData.js
+++ src/singleFormData.js
@@ -9,15 +9,15 @@ const sampleData = [{
             "placeholder": "Enter your first & last name",
             "isRequired": true
         },
-        {
-            "name": "email",
-            "type": "string",
-            "label": "Email ID",
-            "placeholder": "enter your enail ID",
-            "isRequired": true,
-            "pattern": "[A-Za-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}",
-            "errorText": "Invalid Email!"
-        },
+        // {
+        //     "name": "email",
+        //     "type": "string",
+        //     "label": "Email ID",
+        //     "placeholder": "enter your enail ID",
+        //     "isRequired": true,
+        //     "pattern": "[A-Za-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}",
+        //     "errorText": "Invalid Email!"
+        // },
         // {
         //     "name": "phone",
         //     "type": "number",
@@ -27,64 +27,64 @@ const sampleData = [{
         //     "pattern": "[0-9]{10}$",
         //     "errorText": "Enter 10 digit number"
         // },
-        // {
-        //     "name": "languages",
-        //     "type": "select",
-        //     "isMultiple": true,
-        //     "label": "Languages you know : ",
-        //     "errorText": "Select atleast one value",
-        //     "required": true,
-        //     "payload": ["English", "Urdu", "Hindi", "Bengali", "Marathi", "Hebrew"]
-        // },
-        // {
-        //     "name": "interests1",
-        //     "type": "checkboxGroup",
-        //     "label": "Choose your Interests : ",
-        //     "required": true,
-        //     "payload": ["sports", "games", "music"],
-        //     "errorText": "Select atleast one Value"
-        // },
+        {
+            "name": "languages",
+            "type": "select",
+            "isMultiple": true,
+            "label": "Languages you know : ",
+            "errorText": "Select atleast one value",
+            "isRequired": true,
+            "payload": ["English", "Urdu", "Hindi", "Bengali", "Marathi", "Hebrew"]
+        },
+        {
+            "name": "interests1",
+            "type": "checkboxGroup",
+            "label": "Choose your Interests : ",
+            "isRequired": true,
+            "payload": ["sports", "games", "music"],
+            "errorText": "Select atleast one Value"
+        },
         {
             "name": "photo",
             "type": "file",
             "label": "Upload your photo",
             "placeholder": "Only supports images now!",
             "helperText": "Will send the file data back as blob",
-            "required": true,
+            "isRequired": true,
             "errorText": "Upload the correct format!!",
         },
-        // {
-        //     "name": "gender",
-        //     "type": "radioGroup",
-        //     "label": "Gender",
-        //     "required": true,
-        //     "payload": [
-        //         {
-        //             "name": "male",
-        //             "label": "Male",
-        //             "disabled": false,
-        //         },
-        //         {
-        //             "name": "female",
-        //             "label":"Female",
-        //             "disabled": false
-        //         },
-        //         {
-        //             "name": "others",
-        //             "label":"Others",
-        //             "disabled": true
-        //         }
-        //     ]
-        // },
         {
-            "name": "password",
-            "type": "password",
-            "label": "Password",
-            "placeholder": "set your password",
-            "pattern": "[0-9a-zA-Z]{6,}",
-            "isRequired": true,
-            "errorText": "Minimum of 6 charecters is required"
+            "name": "gender",
+            "type": "radioGroup",
+            "label": "Gender",
+            "required": true,
+            "payload": [
+                {
+                    "name": "male",
+                    "label": "Male",
+                    "disabled": false,
+                },
+                {
+                    "name": "female",
+                    "label":"Female",
+                    "disabled": false
+                },
+                {
+                    "name": "others",
+                    "label":"Others",
+                    "disabled": true
+                }
+            ]
         },
+        // {
+        //     "name": "password",
+        //     "type": "password",
+        //     "label": "Password",
+        //     "placeholder": "set your password",
+        //     "pattern": "[0-9a-zA-Z]{6,}",
+        //     "isRequired": true,
+        //     "errorText": "Minimum of 6 charecters is required"
+        // },
         // {
         //     "name": "passwordCheck",
         //     "type": "password",
@@ -92,12 +92,12 @@ const sampleData = [{
         //     "placeholder": "re-enter your password",
         //     "isRequired": true
         // },
-        {
-            "name": "car",
-            "type": "string",
-            "label": "Car Model",
-            "placeholder": "name of the car you have"
-        }
+        // {
+        //     "name": "car",
+        //     "type": "string",
+        //     "label": "Car Model",
+        //     "placeholder": "name of the car you have"
+        // }
     ]}
 ]
 
